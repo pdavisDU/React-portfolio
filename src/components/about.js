de2c7b87ae03471ab30fd8557e5fadf3cce1878c
pdavisDU <pdavisdu@gmail.com>
2022-07-26 13:20:22 -0600

    updated bio as well as fixing the navigation componenet

--- src/components/about.js
+++ src/components/about.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const About = ()=>{
-return(
-    <section className='card' id='about'>
-        <div>
-            <div className='card-body'>
-                <h3 className='card-title'>About Me</h3>
-                <p className='card-text'>I have spent the past 4 years in real estate learning the market and networking with others. I am making a career transition into web development to be able to build and contribute to others projects.</p>
-            </div>
+const About = () => {
+  return (
+    <section className="card" id="about">
+      <div>
+        <div className="card-body">
+          <h3 className="card-title">About Me</h3>
+          <p className="card-text">
+            Welcome to my portfolio! My Name is Paul Davis and I recently
+            finished a full stack coding bootcamp at the University of Denver. I
+            come from a background in real estate, but I was ready for a change
+            in career that would allow me to follow my passion. In this new
+            career path I am able to create and have the freedom to explore many
+            different technologies, as well as continue to learn and challenge
+            myself in an ever evolving field. I feel as though I have finally
+            found a career that allows me to thrive not only as an individual,
+            but within a team.
+          </p>
         </div>
+      </div>
     </section>
-)
-}
+  );
+};
 export default About;
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,31 +1,64 @@
-import React from 'react';
-const Portfolio = ()=>{
-    return(
-        <div id='myPortfolio' className='row'>
-            <div className='column'>
-                <h2>My Portfolio</h2>
-            </div>
-            <div id='liveWork' className='column'>
-                <div id='marvel' className='card'>
-                    <div className='work'>Marvel for beginners</div>
-                    <a href='https://bhodge166.github.io/MarvelForBeginners/' target='_blank'>Live Link</a>
-                    <a href='https://github.com/bhodge166/MarvelForBeginners' target='_blank'>Repo</a>
-                    <img src={require('../images/marvel.png')} alt='Marvel screenshot'></img>
-                </div>
-                <div  id='password' className='card'>
-                    <div className='work'>Password Generator</div>
-                    <a href='https://pdavisdu.github.io/Password-generator/' target='_blank'>Live Link</a>
-                    <a href='https://github.com/pdavisDU/Password-generator' target='_blank'>Repo</a>
-                    <img src={require('../images/pw.png')} alt='Password screenshot'></img>
-                </div>
-                <div id='daily-planner' className='card'>
-                    <div className='work'>Daily Planner</div>
-                    <a href='https://pdavisdu.github.io/daily-planner/' target='_blank'>Live Link</a>
-                    <a href='https://github.com/pdavisDU/daily-planner' target='_blank'>Repo</a>
-                    <img src={require('../images/daily.png')} alt='Planner screenshot'></img>
-                </div>
-            </div>
+import React from "react";
+const Portfolio = () => {
+  return (
+    <div id="myPortfolio" className="row">
+      <div className="column">
+        <h2>My Portfolio</h2>
+      </div>
+      <div id="liveWork" className="column">
+        <div id="marvel" className="card">
+          <div className="work">Marvel for beginners</div>
+          <a
+            href="https://bhodge166.github.io/MarvelForBeginners/"
+            target="_blank"
+          >
+            Live Link
+          </a>
+          <a
+            href="https://github.com/bhodge166/MarvelForBeginners"
+            target="_blank"
+          >
+            Repo
+          </a>
+          <img
+            src={require("../images/marvel.png")}
+            alt="Marvel screenshot"
+          ></img>
         </div>
-    )
-}
+        <div id="password" className="card">
+          <div className="work">Password Generator</div>
+          <a
+            href="https://pdavisdu.github.io/Password-generator/"
+            target="_blank"
+          >
+            Live Link
+          </a>
+          <a
+            href="https://github.com/pdavisDU/Password-generator"
+            target="_blank"
+          >
+            Repo
+          </a>
+          <img
+            src={require("../images/pw.png")}
+            alt="Password screenshot"
+          ></img>
+        </div>
+        <div id="daily-planner" className="card">
+          <div className="work">Daily Planner</div>
+          <a href="https://pdavisdu.github.io/daily-planner/" target="_blank">
+            Live Link
+          </a>
+          <a href="https://github.com/pdavisDU/daily-planner" target="_blank">
+            Repo
+          </a>
+          <img
+            src={require("../images/daily.png")}
+            alt="Planner screenshot"
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Portfolio;
