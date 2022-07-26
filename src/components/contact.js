f8dc4cd5a86d8517372a8e94575213fd02608f14
pdavisDU <pdavisdu@gmail.com>
2022-07-26 12:14:36 -0600

    fixed contact overlap and padding

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,17 +1,31 @@
 import React from "react";
-const Contact = ()=>{
-    return(
-        <div className="container">
-            <form action='action_page.php'>
-                <label for='name'>Name</label>
-                <input type='text' id='myName' className="name" placeholder="John Smith.."></input>
-                <label for='email'>E-mail</label>
-                <input type='text' id='e-mail' className="userEmail" placeholder="Email.."></input>
-                <label for='message'>Leave a message</label>
-                <textarea id='message-area' className="msg" placeholder="Message.."></textarea>
-                <input type='submit' value='submit'></input>
-            </form>
-        </div>
-    )
-}
+const Contact = () => {
+  return (
+    <div className="container">
+      <form action="action_page.php">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="myName"
+          className="name"
+          placeholder="John Smith.."
+        ></input>
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="text"
+          id="e-mail"
+          className="userEmail"
+          placeholder="Email.."
+        ></input>
+        <label htmlFor="message">Leave a message</label>
+        <textarea
+          id="message-area"
+          className="msg"
+          placeholder="Message.."
+        ></textarea>
+        <input type="submit" value="submit"></input>
+      </form>
+    </div>
+  );
+};
 export default Contact;
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,22 @@
-import React from 'react';
-const Footer = () =>{
-    return(
-        <div className='card-body'>
-            <a type='button' className='btn' href='mailto: pdavisDU@gmail.com'>Email</a>
-            <a type='button' className='btn' href='https://www.linkedin.com/in/paul-davis-58663a74/'>LinkedIn</a>
-            <a type='button' className='btn' href='https://github.com/pdavisDU'>Github</a>
-        </div>
-    )
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="card-body">
+      <a type="button" className="btn" href="mailto: pdavisDU@gmail.com">
+        Email
+      </a>
+      <a
+        type="button"
+        className="btn"
+        href="https://www.linkedin.com/in/paul-davis-58663a74/"
+      >
+        LinkedIn
+      </a>
+      <a type="button" className="btn" href="https://github.com/pdavisDU">
+        Github
+      </a>
+    </footer>
+  );
 }
 export default Footer;
