bf11546a2dc5325c95f73b4b076ea632f97fb283
pdavisDU <pdavisdu@gmail.com>
2022-07-27 11:54:51 -0600

    fixed styling and added icons

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,31 +1,86 @@
-import React from "react";
-const Contact = () => {
+import React, { useState } from "react";
+import validator from "validator";
+import "../Styles/about.css";
+import "../Styles/portfolio.css";
+
+// const Contact = () => {
+//   return (
+// <div className="container">
+//   <form action="action_page.php">
+//     <label htmlFor="name">Name</label>
+//     <input
+//       type="text"
+//       id="myName"
+//       className="name"
+//       placeholder="John Smith.."
+//     ></input>
+//     <label htmlFor="email">E-mail</label>
+//     <input
+//       type="text"
+//       id="e-mail"
+//       className="userEmail"
+//       placeholder="Email.."
+//     ></input>
+//     <label htmlFor="message">Leave a message</label>
+//     <textarea
+//       id="message-area"
+//       className="msg"
+//       placeholder="Message.."
+//     ></textarea>
+//     <input type="submit" value="submit"></input>
+//   </form>
+// </div>
+function Contact() {
+  const [emailError, setEmailError] = useState("");
+  const validateEmail = (e) => {
+    var email = e.target.value;
+
+    if (validator.isEmail(email)) {
+      setEmailError("Valid Email :)");
+    } else {
+      setEmailError("Enter valid Email!");
+    }
+  };
+
   return (
-    <div className="container">
-      <form action="action_page.php">
-        <label htmlFor="name">Name</label>
+    <form id="contact">
+      <div className="contact-form">
+        <label for="name">Name</label>
+        <input type="text" className="form-control" placeholder="Bruce Wayne" />
+      </div>
+      <div className="form-group">
+        <label for="email">Email Address</label>
         <input
-          type="text"
-          id="myName"
-          className="name"
-          placeholder="John Smith.."
-        ></input>
-        <label htmlFor="email">E-mail</label>
-        <input
-          type="text"
-          id="e-mail"
-          className="userEmail"
-          placeholder="Email.."
-        ></input>
-        <label htmlFor="message">Leave a message</label>
+          type="email"
+          className="form-control"
+          id="exampleFormControlInput1"
+          placeholder="name@example.com"
+          onChange={(e) => validateEmail(e)}
+        ></input>{" "}
+        <br />
+        <span
+          style={{
+            color: "green",
+          }}
+        >
+          {emailError}
+        </span>
+      </div>
+
+      <div className="form-group">
+        <label for="message">Message</label>
         <textarea
-          id="message-area"
-          className="msg"
-          placeholder="Message.."
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          placeholder="How did you get so awesome?"
+          rows="3"
         ></textarea>
-        <input type="submit" value="submit"></input>
-      </form>
-    </div>
+      </div>
+      <button id="submitBtn" type="submit">
+        Submit
+      </button>
+    </form>
   );
-};
+}
+
 export default Contact;
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,41 @@
 import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { MdOutlineEmail } from "react-icons//md";
+import { BsGithub } from "react-icons/bs";
+import "../Styles/about.css";
+import "../Styles/portfolio.css";
+import "../Styles/footer.css";
 
 function Footer() {
   return (
-    <footer className="card-body">
-      <a type="button" className="btn" href="mailto: pdavisDU@gmail.com">
-        Email
-      </a>
-      <a
-        type="button"
-        className="btn"
-        href="https://www.linkedin.com/in/paul-davis-58663a74/"
-      >
-        LinkedIn
-      </a>
-      <a type="button" className="btn" href="https://github.com/pdavisDU">
-        Github
-      </a>
+    <footer id="footer">
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link" href="mailto: pdavisDU@gmail.com" target="_blank">
+            {" "}
+            <MdOutlineEmail />
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            href="https://www.linkedin.com/in/paul-davis-58663a74/"
+            target="_blank"
+          >
+            {" "}
+            <BsLinkedin />{" "}
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            href="https://github.com/pdavisDU"
+            target="_blank"
+          >
+            <BsGithub />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
